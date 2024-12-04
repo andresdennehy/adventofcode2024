f533067a0f69b11bc361677198066d95d71036d1
andres-dennehy-oeg <andres.dennehy@octoenergy.com>
2024-12-04 09:07:36 +0100

    Day 4

---
+++ day04/solution1.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func checkForXmas(letters []string, row, col int, deltaY, deltaX int) bool {
+	var spells string
+	for i := 0; i < 4; i++ {
+		if row < 0 || row > len(letters)-1 {
+			return false
+		}
+		if col < 0 || col > len(letters[0])-1 {
+			return false
+		}
+		spells += string(letters[row][col])
+		row += deltaY
+		col += deltaX
+	}
+	if spells == "XMAS" {
+		return true
+	}
+	return false
+}
+
+func main() {
+
+	file, err := os.Open("input.txt")
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	letters := make([]string, 0)
+	var result int
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		letters = append(letters, line)
+	}
+
+	// Assuming correct input
+	height := len(letters)
+	width := len(letters[0])
+	directions := [][]int{{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}}
+	// Run four operations per letter in the letters slice
+	// to check for XMAS. This is O(4n) = O(n)
+	for i := 0; i < height; i++ {
+		for j := 0; j < width; j++ {
+			for _, direction := range directions {
+				if checkForXmas(letters, i, j, direction[0], direction[1]) {
+					result++
+				}
+			}
+		}
+	}
+
+	fmt.Printf("Result is %v\n", result)
+}
---
+++ day04/solution2.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func checkForMas(letters []string, row, col int) bool {
+	if row < 1 || row > len(letters)-2 {
+		return false
+	}
+	if col < 1 || col > len(letters[0])-2 {
+		return false
+	}
+	firstDiagonal := string([]byte{letters[row-1][col-1], letters[row][col], letters[row+1][col+1]})
+	secondDiagonal := string([]byte{letters[row+1][col-1], letters[row][col], letters[row-1][col+1]})
+	if (firstDiagonal == "MAS" || firstDiagonal == "SAM") && (secondDiagonal == "MAS" || secondDiagonal == "SAM") {
+		return true
+	}
+	return false
+}
+
+func main() {
+
+	file, err := os.Open("input.txt")
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	letters := make([]string, 0)
+	var result int
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		letters = append(letters, line)
+	}
+
+	// Assuming correct input
+	height := len(letters)
+	width := len(letters[0])
+	for i := 0; i < height; i++ {
+		for j := 0; j < width; j++ {
+			if checkForMas(letters, i, j) {
+				result++
+			}
+		}
+	}
+
+	fmt.Printf("Result is %v\n", result)
+}
